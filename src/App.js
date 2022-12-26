fcb3e066daafe10c7139010e0c8a0cf80c81468c
M Irfan <naprirfan@gmail.com>
2022-12-26 10:36:23 +0800

    feat(text): change react description

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed
+          This was from feature branch
         </p>
         <a
           className="App-link"
